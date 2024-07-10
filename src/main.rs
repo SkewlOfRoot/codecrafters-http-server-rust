cc091cbabc90bd19b9285867f5c290562ea4e04f
Andreas Giereck Kristiansen <giereck@hotmail.com>
2024-07-10 22:14:42 +0200

    Handle user-agent route.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
+use itertools::Itertools;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -8,18 +10,9 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 let request = read_request(&mut _stream).unwrap();
+                println!("REQUEST:\r\n{:#?}", request);
 
-                let response: HttpResponse;
-                if request.request_path == "/" {
-                    response = HttpResponse::ok(None);
-                } else if request.request_path.starts_with("/echo") {
-                    let value = request.request_path.split('/').last().unwrap();
-                    response = HttpResponse::ok(Some(value.to_string()));
-                } else {
-                    response = HttpResponse::not_found()
-                }
-
-                println!("{:#?}", request);
+                let response = generate_response(&request);
                 write_response(_stream, response);
             }
             Err(e) => {
@@ -36,10 +29,47 @@ fn read_request(_stream: &mut TcpStream) -> Result<HttpRequest, String> {
             if let Ok(request) = String::from_utf8(read_buff[..bytes_read].to_vec()) {
                 Ok(HttpRequest::from_str(&request))
             } else {
-                Err(String::from("Reveived non-UTF8 data."))
+                Err(String::from("Received non-UTF8 data."))
             }
         }
         Err(e) => Err(format!("Failed to read bytes from stream: {}", e)),
+    }
+}
+
+fn generate_response(request: &HttpRequest) -> HttpResponse {
+    let response: HttpResponse;
+    if request.request_path == "/" {
+        response = gen_root_response();
+    } else if request.request_path.starts_with("/echo") {
+        response = gen_echo_response(request);
+    } else if request.request_path == "/user-agent" {
+        response = gen_user_agent_response(request);
+    } else {
+        response = HttpResponse::not_found()
+    }
+    response
+}
+
+fn gen_root_response() -> HttpResponse {
+    HttpResponse::ok(None)
+}
+
+fn gen_echo_response(request: &HttpRequest) -> HttpResponse {
+    let value = request.request_path.split('/').last().unwrap();
+    HttpResponse::ok(Some(value.to_string()))
+}
+
+fn gen_user_agent_response(request: &HttpRequest) -> HttpResponse {
+    let collect_vec = &request
+        .headers
+        .iter()
+        .filter(|x| x.name.to_lowercase() == "user-agent")
+        .collect_vec();
+    let user_agent = collect_vec.first();
+    if let Some(u) = user_agent {
+        HttpResponse::ok(Some(String::from(&u.value)))
+    } else {
+        HttpResponse::not_found()
     }
 }
 
@@ -53,6 +83,7 @@ fn write_response(mut _stream: TcpStream, response: HttpResponse) {
 struct HttpRequest {
     request_type: HttpRequestType,
     request_path: String,
+    headers: Vec<HttpHeader>,
 }
 
 #[derive(Debug)]
@@ -66,11 +97,26 @@ enum HttpRequestType {
 
 impl HttpRequest {
     fn from_str(request_str: &str) -> HttpRequest {
-        let first_line = request_str.split('\n').next().unwrap();
+        let lines = &mut request_str.split('\n');
+        let first_line = lines.next().unwrap();
         let mut values = first_line.split(' ');
+        let mut headers: Vec<HttpHeader> = Vec::new();
+
+        for header_line in lines {
+            if header_line == "\r" {
+                break;
+            }
+            let header_vals = header_line.splitn(2, ':').map(|x| x.trim()).collect_vec();
+            headers.push(HttpHeader::new(
+                header_vals.first().unwrap(),
+                header_vals.last().unwrap(),
+            ))
+        }
+
         HttpRequest {
             request_type: HttpRequestType::from_str(values.next().unwrap()).unwrap(),
             request_path: values.next().unwrap().to_string(),
+            headers,
         }
     }
 }
@@ -96,7 +142,7 @@ struct HttpResponse {
     version: String,
     status_code: HttpStatusCode,
     headers: Vec<HttpHeader>,
-    body: Option<String>,
+    body: String,
 }
 
 struct HttpStatusCode {
@@ -104,6 +150,7 @@ struct HttpStatusCode {
     description: String,
 }
 
+#[derive(Debug)]
 struct HttpHeader {
     name: String,
     value: String,
@@ -124,18 +171,12 @@ impl HttpHeader {
 
 impl HttpResponse {
     fn ok(body: Option<String>) -> HttpResponse {
-        let content_length: usize;
-
-        if let Some(b) = &body {
-            content_length = b.len();
-        } else {
-            content_length = 0;
-        }
+        let body = body.unwrap_or_default();
 
         let mut headers: Vec<HttpHeader> = vec![HttpHeader::new("Content-Type", "text/plain")];
         headers.push(HttpHeader::new(
             "Content-Length",
-            content_length.to_string().as_str(),
+            body.len().to_string().as_str(),
         ));
 
         HttpResponse {
@@ -157,7 +198,7 @@ impl HttpResponse {
                 description: String::from("Not Found"),
             },
             headers: Vec::new(),
-            body: None,
+            body: String::new(),
         }
     }
 
@@ -174,11 +215,11 @@ impl HttpResponse {
         let mut response_str = response_lines.join("\r\n");
         response_str.push_str("\r\n\r\n");
 
-        if self.body.is_some() {
-            response_str.push_str(self.body.unwrap().as_str());
+        if !self.body.is_empty() {
+            response_str.push_str(self.body.as_str());
         }
 
-        println!("{}", response_str);
+        println!("RESPONSE:\r\n{}", response_str);
         response_str.as_bytes().to_vec()
     }
 }
